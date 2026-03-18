d139b8c657883943f9dd8350c884f1c26b5af096
AnastasiaCherniakh <anastayshablake@gmail.com>
2026-03-18 21:40:07 +0100

    feat: add sorting functionality to dashboard

---
+++ src/components/SortControls.tsx
@@ -0,0 +1,29 @@
+import type { SortOption } from "../utils/sortIncidents"
+
+/**
+ * UI controls for selecting sorting order.
+ */
+
+type Props = {
+    sortBy: SortOption
+    onSortChange: (value: SortOption) => void
+}
+
+export default function SortControls({ sortBy, onSortChange }: Props){
+    return (
+        <div>
+
+            <label>Sort By</label>
+
+            <select
+            value={sortBy}
+            onChange={(e) => onSortChange(e.target.value as SortOption)}>
+                <option value="date-desc">Newest First</option>
+                <option value="date-asc">Oldest First</option>
+                <option value="severity-decs">Highest Severity</option>
+                <option value="severity-asc">Lowest Severity</option>
+            </select>
+
+        </div>
+    )
+}
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,7 +2,10 @@ import { useState } from "react"
 import { useIncidents } from "../hooks/useIncidents"
 import IncidentTable from "../components/IncidentTable"
 import FilterControls from "../components/FilterControls"
+import SortControls from "../components/SortControls"
 import { filterIncidents } from "../utils/filterIncidents"
+import { sortIncidents } from "../utils/sortIncidents"
+import type { SortOption } from "../utils/sortIncidents"
 import type { Severity, Status } from "../types/incident"
 
 /**
@@ -15,11 +18,16 @@ export default function DashboardPage() {
 
     const [severityFilter, setSeverityFilter] = useState<Severity | ''>('');
     const [statusFilter, setStatusFilter] = useState<Status | ''>('');
+    const [sortBy, setSortBy] = useState<SortOption>('date-desc')
 
+    // First step: filter
     const filteredIncidents = filterIncidents(incidents, {
         severity: severityFilter || undefined,
         status: statusFilter || undefined
     });
+
+    // Second step: sort
+    const sortedIncidents = sortIncidents(filteredIncidents, sortBy); 
 
     return (
         <div>
@@ -33,7 +41,11 @@ export default function DashboardPage() {
             onStatusChange={setStatusFilter}
             />
 
-            <IncidentTable incidents={filteredIncidents}/>
+            <SortControls 
+            sortBy={sortBy}
+            onSortChange={setSortBy}/>
+
+            <IncidentTable incidents={sortedIncidents}/>
 
         </div>
     )
