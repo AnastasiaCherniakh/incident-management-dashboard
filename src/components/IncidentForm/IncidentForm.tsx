b905ca95a0a9989111f45733abe5221b8a84e416
AnastasiaCherniakh <anastayshablake@gmail.com>
2026-04-01 15:40:36 +0200

    ux: allow form submission and display validation errors instead of disabling button

--- src/components/IncidentForm/IncidentForm.tsx
+++ src/components/IncidentForm/IncidentForm.tsx
@@ -135,7 +135,6 @@ export default function IncidentForm({ onSubmit }: Props) {
             <button 
             type="submit"
             className="button-primary center-button"
-            disabled={!formData.title || !formData.severity || !formData.category} //disable submit if required fields are empty
             >Report Incident
             </button>
         </form>
